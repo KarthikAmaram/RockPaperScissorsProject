f1f97cb639847a224a93d40d5a2cb0e7a5d8067a
Karthik Amaram <amaramkart09@gmail.com>
2025-05-31 01:00:47 -0500

    Coded human choice and added a scorekeeper var

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
-    let randNum = Math.random()
-    console.log(randNum)
+    let randNum = Math.random();
+
     if (randNum <= 0.33) {
-        let choice = "rock"
-        return choice
+        let choice = "rock";
+        return choice;
     }
     else if (randNum <= 0.66) {
-        let choice = "paper"
-        return choice
+        let choice = "paper";
+        return choice;
     }
     else {
-        let choice = "scissors"
-        return choice
+        let choice = "scissors";
+        return choice;
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let choice = prompt("Pick rock, paper, or scissors: ");
+    return choice;
+}
+
+console.log(getHumanChoice());
